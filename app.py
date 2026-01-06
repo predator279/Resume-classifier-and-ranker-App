fd8e07880cab2e80c39550cbf111aacdbd2749f6
predator279 <126012425+predator279@users.noreply.github.com>
2026-01-06 14:38:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,10 +211,6 @@ st.set_page_config(page_title="Resume Analyzer", layout="centered")
 st.title("🔍 AI-Powered Resume Analyzer")
 st.write("CUDA available:", torch.cuda.is_available())
 
-if torch.cuda.is_available():
-    st.write("Running on GPU:", torch.cuda.get_device_name(0))
-else:
-    st.write("Running on CPU")
 # Sidebar for navigation
 mode = st.sidebar.radio("Choose Mode:", ["Resume Category Prediction", "Resume Ranking"])
 
@@ -359,6 +355,7 @@ elif mode == "Resume Ranking":
                         req_display = req_data['text'][:70].replace('\n', ' ') + "..."
                         
                         st.markdown(f"{icon} **{req_type.capitalize()}** (`{req_display}`): **{req_data['score']:.4f}**")
+
 
 
 
