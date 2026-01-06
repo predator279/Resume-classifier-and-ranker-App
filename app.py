34079b48dec30833d34b3ea6392677ca85e9b6ac
predator279 <126012425+predator279@users.noreply.github.com>
2026-01-06 14:18:42 +0530

    adding inference message cpu or gpu
    st.write("CUDA available:", torch.cuda.is_available())
    
    if torch.cuda.is_available():
        st.write("Running on GPU:", torch.cuda.get_device_name(0))
    else:
        st.write("Running on CPU")

--- app.py
+++ app.py
@@ -207,6 +207,14 @@ def rank_resumes_bert(job_structure, job_description_full, uploaded_files, model
 
 
 # === Streamlit UI ===
+
+st.write("CUDA available:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    st.write("Running on GPU:", torch.cuda.get_device_name(0))
+else:
+    st.write("Running on CPU")
+    
 st.set_page_config(page_title="Resume Analyzer", layout="centered")
 st.title("🔍 AI-Powered Resume Analyzer")
 
@@ -354,5 +362,6 @@ elif mode == "Resume Ranking":
                         req_display = req_data['text'][:70].replace('\n', ' ') + "..."
                         
                         st.markdown(f"{icon} **{req_type.capitalize()}** (`{req_display}`): **{req_data['score']:.4f}**")
+
 
 
