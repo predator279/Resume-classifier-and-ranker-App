43e6ebea04beb1e379e9adb8ed2f2e9ac3c62309
predator279 <126012425+predator279@users.noreply.github.com>
2026-01-06 14:27:19 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -208,7 +208,7 @@ def rank_resumes_bert(job_structure, job_description_full, uploaded_files, model
 
 # === Streamlit UI ===
 st.set_page_config(page_title="Resume Analyzer", layout="centered")
-st.title("🔍 AI-Powered Resume Analyzer")
+st.title("🔍 AI-Powered Resume Analyzer PPP")
 
 # Sidebar for navigation
 mode = st.sidebar.radio("Choose Mode:", ["Resume Category Prediction", "Resume Ranking"])
